11dcf348efdc46eb5f830a5302b164c4c426f5c2
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-20 00:24:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,22 +66,24 @@ programmer_hash:yukihiro_matsumoto => {
  }
 end
 
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        }
-				:alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-    }
-end
+programmer_hash =
+	{
+			:grace_hopper => {
+				:known_for => "COBOL",
+				:languages => ["COBOL", "FORTRAN"]
+			},
+			:alan_kay => {
+				:known_for => "Object Orientation",
+				:languages => ["Smalltalk", "LISP"]
+			},
+			:dennis_ritchie => {
+				:known_for => "Unix",
+				:languages => ["C"]
+			}
+	 }
+
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
 
 def changing_alan
 	programmer_hash =
