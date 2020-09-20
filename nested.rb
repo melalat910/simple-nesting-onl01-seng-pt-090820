5725d0a2623a9482ebd078c2e1314c817d2f590b
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-20 00:05:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,11 +61,11 @@ def adding_matz
 # add the following information to the top level of programmer_hash
 programmer_hash:yukihiro_matsumoto => {
 	:known_for => "Ruby",
-	:languages => ["LISP", "C"]]
+	:languages => ["LISP", "C"]
 
 	puts programmer_hash
-	}
 }
+end
 end
 
 	programmer_hash =
