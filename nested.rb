09bbf0f4fd93d6b7635272296fc748d7432ecd6c
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-20 00:20:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -72,10 +72,10 @@ end
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         }
-        :alan_kay => {
+				:alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
-        }
+        },
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
